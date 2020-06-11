f2c3b503b5ed747e016774d4593c70541ce906f3
pedro-mendonca <ped.gaspar@gmail.com>
2020-06-11 17:06:23 +0100

    Add icons

---
+++ assets/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Intentionally empty file.
+ *
+ * It exists to stop directory listings on poorly configured servers.
+ *
+ * @package    Translation Tools
+ */
+
+// Silence is golden.
